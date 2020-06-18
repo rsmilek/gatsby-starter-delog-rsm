1884e1b06e01f9345a4d19b721e15e3825ebe3cc
rsmilek <smilek.radim@gmail.com>
2020-06-18 11:48:16 +0200

    + index2 page fully working by bootstrap-creative-gh-pages example

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,12 @@
 // Styles for DELOG
-import "./src/styles/global.scss";
+// import "./src/styles/global.scss";
 // Styles for BOOTSTRAP STARTER
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./src/styles/global2.css";
+// import "./src/styles/global2.css";
+// import $ from "jquery";
+// import "jquery/dist/jquery.min.js";
+// import "./src/jqueryLoader.js";
+import "./src/styles/index.css";
+import "magnific-popup/dist/magnific-popup.css"; // Third party plugin CSS
+import "./src/css/styles.css"; // Core theme CSS (includes Bootstrap)
+import "bootstrap/dist/js/bootstrap.bundle.min"; // Bootstrap core JS
