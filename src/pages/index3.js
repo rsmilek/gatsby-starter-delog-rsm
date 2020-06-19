a5f856d29fd1621d6f9e727b9825540c9d77d5f4
rsmilek <smilek.radim@gmail.com>
2020-06-19 10:11:06 +0200

    * reactoring of pages: /index2=creative-gh-pages, /index3=myNavbar

--- src/pages/index3.js
+++ src/pages/index3.js
@@ -1,26 +1,24 @@
 import React from "react";
 import Helmet from "react-helmet";
 import { graphql } from "gatsby";
-import CreativeGh from "../components/creativeGh.js";
+import MyNavBar from "../components/myNavbar.js";
 
-const index2Page = ({ data: { site } }) => {
+const index3Page = ({ data: { site } }) => {
   return (
     <div>
       <Helmet>
         <title>Contact - {site.siteMetadata.title}</title>
         <meta name="description" content={site.siteMetadata.description} />
-        {/* <body className="new-class-for-body" /> */}
-        {/* <body data-spy="scroll" /> */}
       </Helmet>
-      <CreativeGh />
+      <MyNavBar />
     </div>
   );
 };
 
-export default index2Page;
+export default index3Page;
 
 export const pageQuery = graphql`
-  query Index2PageQuery {
+  query Index3PageQuery {
     site {
       siteMetadata {
         title
